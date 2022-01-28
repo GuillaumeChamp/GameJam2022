26258c051e8a0691fc3146f79012d51ae3b501bc
GuillaumeChamp <guillaume.champtoussel@ensea.fr>
2022-01-28 10:47:53 +0100

    Adding controller

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -6,7 +6,9 @@ import javafx.scene.input.KeyCode;
 import java.util.ArrayList;
 
 public class Controller {
+
     public static ArrayList<KeyCode> inputString;
+
     public static void input(KeyCode code){
         //here add exceptional input with if(code==KeyCode.X) ... break;
         if (!inputString.contains(code)) inputString.add(code);
@@ -15,6 +17,10 @@ public class Controller {
         inputString.remove(code);
     }
 
+    /**
+     * function which move the player according to input and behaviour
+     * @param player the player
+     */
     public static void action(Player player){
 
     }
